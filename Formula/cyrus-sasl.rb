a7268599841e9996a88504008c27c1ffe9db9851
Liam Rendell <liam.rendell@gmail.com>
2018-12-18 16:17:16 +1300

    First working attempt. Cleanup needed.

--- Formula/cyrus-sasl.rb
+++ Formula/cyrus-sasl.rb
@@ -6,7 +6,7 @@ class CyrusSasl < Formula
   homepage ""
   url "ftp://ftp.cyrusimap.org/cyrus-sasl//cyrus-sasl-2.1.27.tar.gz"
   sha256 "26866b1549b00ffd020f188a43c258017fa1c382b3ddadd8201536f72efb05d5"
-  # depends_on "cmake" => :build
+  #depends_on "cmake" => :build
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
@@ -14,12 +14,15 @@ class CyrusSasl < Formula
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
+                          "--disable-macos-framework", # as this is hardcoded to write to protected /library
                           "--prefix=#{prefix}"
     # system "cmake", ".", *std_cmake_args
     system "make", "install" # if this fails, try separate make/make install steps
   end
 
-  test do
+  keg_only :provided_by_osx
+
+#  test do
     # `test do` will create, run in and delete a temporary directory.
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
@@ -29,6 +32,6 @@ class CyrusSasl < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
+#    system "false"
+#  end
 end
